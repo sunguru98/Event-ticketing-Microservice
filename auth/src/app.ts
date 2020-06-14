f449b19eecdfd07eb12f0402b13addfe13143c97
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2020-06-14 19:15:55 +0530

    Added dependencies for testing also isolated server listening to a separate file

---
+++ auth/src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import appRouter from "./auth.router";
+import errorHandler from "./middlewares/errorHandler";
+import NotFoundError from "./utils/errors/NotFoundError";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    sameSite: true,
+    secure: true,
+    signed: false
+  })
+);
+
+app.use("/api/users", appRouter);
+app.all("*", async req => {
+  throw new NotFoundError(`${req.url} not found`);
+});
+
+// Error handler
+app.use(errorHandler);
+export default app;
